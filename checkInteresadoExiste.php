84c2ad9d84fe00269693f91c7877a6daa0ae50ef
Eze BIT <efigueroa@bit.com.ar>
2016-03-31 11:58:20 -0300

    Terminado interesado

--- checkInteresadoExiste.php
+++ checkInteresadoExiste.php
@@ -4,7 +4,7 @@ include_once 'conexionCursosExtension.php';
 
 $dni = $_REQUEST['dni'];
 
-$c = "SELECT count(id) as contar FROM interesado WHERE dni='$dni';";
+$c = "SELECT count(id_interesado) as contar FROM interesado WHERE dni='$dni';";
 $s = pg_query($c);
 $r = pg_fetch_array($s);
 
@@ -15,32 +15,85 @@ if($r['contar'] != 0)
 	$cInteresado = "SELECT * FROM interesado WHERE dni='$dni';";
 	$sInteresado = pg_query($cInteresado);
 	$rInteresado = pg_fetch_array($sInteresado);
-	$nombre = $rInteresado['nombre'];
-	$apellido = $rInteresado['apellido'];
-	$direccion = $rInteresado['direccion'];
-	$numero = $rInteresado['numero'];
-	$caracteristicaCasa = $rInteresado['caracteristicaCasa'];
-	$telefonoCasa = $rInteresado['telefonoCasa'];
-	$caracteristicaCel = $rInteresado['caracteristicaCel'];
-	$telefonoCel = $rInteresado['telefonoCel'];
-	$mail = $rInteresado['mail'];
+	$nombre = empty($rInteresado['nombre']) ? "" : $rInteresado['nombre'];
+	$apellido = empty($rInteresado['apellido']) ? "" : $rInteresado['apellido'];
+	$direccion = empty($rInteresado['direccion']) ? "" : $rInteresado['direccion'];
+	$numero = empty($rInteresado['numero']) ? "" : $rInteresado['numero'];
+	$caracteristicaCasa = empty($rInteresado['caracteristicacasa']) ? "" : $rInteresado['caracteristicacasa'];
+	$telefonoCasa = empty($rInteresado['telefonocasa']) ? "" : $rInteresado['telefonocasa'];
+	$caracteristicaCel = empty($rInteresado['caracteristicacel']) ? "" : $rInteresado['caracteristicacel'];
+	$telefonoCel = empty($rInteresado['telefonocel']) ? "" : $rInteresado['telefonocel'];
+	$mail = empty($rInteresado['mail']) ? "" : $rInteresado['mail'];
 }
 else
 {
+	$c1 = "SELECT count(id_inscripto) as contar FROM inscripto WHERE dni='$dni';";
+	$s1 = pg_query($c1);
+	$r1 = pg_fetch_array($s1);
+	if($r1['contar'] != 0)
+	{
+		$esInscripto = 1;
+		$enSistema = 1;
+		$cInscripto = "SELECT * FROM inscripto WHERE dni='$dni';";
+		$sInscripto = pg_query($cInscripto);
+		$rInscripto = pg_fetch_array($sInscripto);
+		$nombre = empty($rInscripto['nombre']) ? "" : $rInscripto['nombre'];
+		$apellido = empty($rInscripto['apellido']) ? "" : $rInscripto['apellido'];
+		$direccion = empty($rInscripto['direccion']) ? "" : $rInscripto['direccion'];
+		$numero = empty($rInscripto['numero']) ? "" : $rInscripto['numero'];
+		$telCasaNoFormat = empty($rInscripto['telfijo']) ? "" : $rInscripto['telfijo'];
+		$telCelNoFormat = empty($rInscripto['telcel']) ? "" : $rInscripto['telcel'];
 
+		$caracteristicaCasa = "";
+		$telefonoCasa = "";
+		$caracteristicaCel = "";
+		$telefonoCel = "";
+
+		$mail = empty($rInscripto['mail']) ? "" : $rInscripto['mail'];
+		/*
+		nombre text,
+		apellido text,
+		telfijo text,
+		mail text,
+		direccion text,
+		numero integer,
+		dni integer,
+		fechainscripto date,
+		fk_tipodoc integer,
+		localidad text,
+		telcel text,
+		*/
+	}
+	else
+	{
+		$esInscripto = 0;
+		$enSistema = 0;
+		$nombre = "";
+		$apellido = "";
+		$direccion = "";
+		$numero = "";
+		$caracteristicaCasa = "";
+		$telefonoCasa = "";
+		$caracteristicaCel = "";
+		$telefonoCel = "";
+		$mail = "";
+	}
 }
 
-/*
-datos.enSistema
-datos.esInscripto
-datos.enSistema
-datos.nombre
-datos.apellido
-datos.direccion
-datos.numero
-datos.caracteristica
-datos.telefono
-datos.mail
-*/
+$outJson = '[{
+		"esInscripto":"'.$esInscripto.'",
+		"enSistema":"'.$enSistema.'",
+		"nombre":"'.$nombre.'",
+		"apellido":"'.$apellido.'",
+		"direccion":"'.$direccion.'",
+		"numero":"'.$numero.'",
+		"caracteristicaCasa":"'.$caracteristicaCasa.'",
+		"telefonoCasa":"'.$telefonoCasa.'",
+		"caracteristicaCel":"'.$caracteristicaCel.'",
+		"telefonoCel":"'.$telefonoCel.'",
+		"mail":"'.$mail.'"
+	}]';
+
+echo $outJson;
 
 ?>
--- guardarinteresado.php
+++ guardarinteresado.php
@@ -1,71 +1,103 @@
 <?php
 include "conexionCursosExtension.php";
-$nombre = empty($_POST['nombre']) ? '' : $_POST['nombre'];
-$apellido = empty($_POST['apellido']) ? '' : $_POST['apellido'];
-$caracteristica = empty($_POST['caracteristica']) ? '' : $_POST['caracteristica'];
-$telefono = empty($_POST['telefono']) ? '' : $_POST['telefono'];
+$dni = empty($_POST['dni']) ? '' : $_POST['dni'];
+$nombre = empty($_POST['nombre']) ? '' : ucwords(strtolower($_POST['nombre']));
+$apellido = empty($_POST['apellido']) ? '' : ucwords(strtolower($_POST['apellido']));
 $mail = empty($_POST['mail']) ? '' : $_POST['mail'];
-$direccion = empty($_POST['direccion']) ? '' : $_POST['direccion'];
+$direccion = empty($_POST['direccion']) ? '' : ucwords(strtolower($_POST['direccion']));
 $numero = empty($_POST['numero']) ? '' : $_POST['numero'];
+
+$estaEnSistema = $_POST['estaEnSistema'];// ? '' : $_POST['estaEnSistema'];
+$esInscripto = $_POST['esInscripto'];// ? '' : $_POST['esInscripto'];
+$cantCursos = empty($_POST['cantCursos']) ? '' : $_POST['cantCursos'];
+$caracteristicaCasa = empty($_POST['caracteristicaCasa']) ? '' : $_POST['caracteristicaCasa'];
+$telefonoCasa = empty($_POST['telefonoCasa']) ? '' : $_POST['telefonoCasa'];
+$caracteristicaCel = empty($_POST['caracteristicaCel']) ? '' : $_POST['caracteristicaCel'];
+$telefonoCel = empty($_POST['telefonoCel']) ? '' : $_POST['telefonoCel'];
 
 $otros = empty($_POST['otros']) ? '' : $_POST['otros'];
 
-$tip2 = pg_query("SELECT COUNT(id_interesado) as max FROM interesado where interesado.mail='".$_POST['mail']."';");
-	$row2 = pg_fetch_array($tip2);
-//if ($row2["contar"]==0){	
-    $tip29 = pg_query($conn,"SELECT MAX(id_interesado) FROM interesado;");
-	$row29 = pg_fetch_array($tip29);
-	$intmax=$row29["max"];
-	$id_int=$intmax+1 ;
-	$sql = "BEGIN TRANSACTION";
-   	$result = pg_query($conn,$sql) or die("Error en el query: $query. " . pg_last_error($conn)); 	
-	
-//INGRESO LA TABLA "ingresante"		
+//Si exites y es inscripto crear nuevo interesado
+//Si no exites crear nuevo interesado
+//Si existe y no es inscripto hacer un update
+//Obtener id del interesado
+//Traer los cursos
+//Ver si otro tiene nombre y crear uno nuevo
+//Agregar id cursos e id interesado a tabla intermedia
 
-	$insert="INSERT INTO interesado(id_interesado, nombre, apellido, telefono, mail, direccion, numero, anio,descripcion)
-VALUES  ('$id_int','$nombre','$apellido','$codigo$telefono','$mail','$direccion',$numero,".date(Y).",'$descripcion');";
-	
-	//echo $insert;
-	$datos=$insert;
-	$res=pg_query($conn,$datos);
-	
-			//$result2 = pg_query($conn,$inspar);
-		//controlo que todo haya sido guardado correctamente	
-//}
+$idInt = 0;
+if($esInscripto == "1")
+{
+	$cIdInt = "SELECT max(id_interesado) FROM interesado;";
+	$sIdInt = pg_query($cIdInt);
+	$rIdInt = pg_fetch_array($sIdInt);
+	$idInt = $rIdInt['max'] + 1;
 
-foreach($_POST['tipocur'] as $v){
-//	$tip56 = pg_query($conn , "SELECT COUNT(interesado.id) as contar FROM interesadoxcurso, interesado where interesado.mail='".$_POST['mail']."' and fk_tipocurso=$v and fk_interesado=interesado.id;");
-//	$row56 = pg_fetch_array($tip56);
-	//if ($row56["contar"]==0){	
-	//	$tip = pg_query($conn,"SELECT id from interesado where interesado.mail='".$_POST['mail']."';");
-		
-		//while($row = pg_fetch_array($tip)){
-			
-		 		$id=$id_int;//$row["id"];
-				$insert2="INSERT INTO interesadoxcurso(fk_interesado,fk_curso) VALUES ('$id','$v');";
-				echo $insert2;
-				$datos2=$insert2;
-				$res2=pg_query($conn,$datos2);
-//		 }	
-//	}
+	$cCrearInt = "INSERT INTO interesado(id_interesado,nombre,apellido,direccion,numero,caracteristicaCasa,telefonoCasa,caracteristicaCel,telefonoCel,dni) VALUES('$idInt','$nombre','$apellido','$direccion','$numero','$caracteristicaCasa','$telefonoCasa','$caracteristicaCel','$telefonoCel','$dni');";
 }
-$error=0;
-	if (!$res & !res2){
-     	$errorpg = pg_last_error($conn);
-     	$termino = "ROLLBACK";
-     	$error=1;
+else if($esInscripto == "0")
+{
+	$cIdInt = "SELECT id_interesado FROM interesado WHERE dni='$dni';";
+	$sIdInt = pg_query($cIdInt);
+	$rIdInt = pg_fetch_array($sIdInt);
+	$idInt = $rIdInt['id_interesado'];
+
+	$cCrearInt = "UPDATE interesado SET nombre='$nombre',apellido='$apellido',direccion='$direccion',numero='$numero',caracteristicaCasa='$caracteristicaCasa',telefonoCasa='$telefonoCasa',caracteristicaCel='$caracteristicaCel',telefonoCel='$telefonoCel',mail='$mail' WHERE id_interesado='$idInt';";
+}
+
+$cCrearCurso = "";
+
+//echo $cantCursos;
+for($i = 1; $i <= $cantCursos; $i++)
+{
+	$nombreIdCurso = 'curso'.$i;
+	$idCurso = $_POST[$nombreIdCurso];
+	if($idCurso != "0")
+	{
+		$cCrearCurso .= "INSERT INTO interesadoxcurso(interesado_fk,curso_fk) (SELECT '$idInt','$idCurso' WHERE NOT EXISTS(SELECT 1 FROM interesadoxcurso WHERE interesado_fk='$idInt' AND curso_fk='$idCurso'));";
 	}
-    else{
-     	$termino = "COMMIT";
-    }
+}
+
+if(trim($otros) != "")
+{
+	$otros = strtolower($otros);
+	$nombreCursoCrear = ucwords($otros);
+	$cCurso = "SELECT count(id) as contar, id FROM cursointeresado WHERE lower(nombre)='$otros' GROUP BY id;";
+	$sCurso = pg_query($cCurso);
+	$rCurso = pg_fetch_array($sCurso);
+	if($rCurso['contar'] == 0)
+	{
+		$cIdCurso = "SELECT max(id) FROM cursointeresado;";
+		$sIdCurso = pg_query($cIdCurso);
+		$rIdCurso = pg_fetch_array($sIdCurso);
+		$idCursoCrear = $rIdCurso['max'] + 1;
+
+		$cCrearCurso .= "INSERT INTO cursointeresado(id,nombre) VALUES('$idCursoCrear','$nombreCursoCrear'); INSERT INTO interesadoxcurso(interesado_fk,curso_fk) VALUES('$idInt','$idCursoCrear');";
+	}
+	else
+	{
+		$idCursoAgregar = $rCurso['id'];
+		$cCrearCurso .= "INSERT INTO interesadoxcurso(interesado_fk,curso_fk) (SELECT '$idInt','$idCursoAgregar' WHERE NOT EXISTS(SELECT 1 FROM interesadoxcurso WHERE interesado_fk='$idInt' AND curso_fk='$idCursoAgregar'));";
+	}
+}
+
+$sqlGuardar = $cCrearInt.$cCrearCurso;
+//echo $sqlGuardar;
+
+$error = 0;
+if (!pg_query($sqlGuardar)){
+	$errorpg = pg_last_error($conn);
+	$termino = "ROLLBACK";
+	$error=1;
+}else{
+	$termino = "COMMIT";
+}
 pg_query($termino);
+
 if ($error==1){
-	echo '<script language="JavaScript"> 
-		alert("Los datos no se guardaron correctamente. Pongase en contacto con el administrador");</script>';
+	echo '<script language="JavaScript"> alert("Los datos no se modificaron correctamente. Pongase en contacto con el administrador");window.location="interesado.html"</script>';
+}else{
+	echo '<script language="JavaScript"> alert("Los datos se actualizaron correctamente."); window.location = "interesado.html";</script>';
 }
-else{
-	echo '<script language="JavaScript"> 
-		alert("Los datos se guardaron correctamente.");</script>';
-}
-echo '<script language="JavaScript"> 	location ="inscripcion.php"	</script>';
+
 ?>
